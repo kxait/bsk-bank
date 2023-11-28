88c532b475e243bce80924eea01f44d0ff11afe3
Tomek Osika <tomasz.osika@protonmail.com>
2023-11-28 23:30:00 +0100

    uhhh

---
+++ lib/initDb.go
@@ -0,0 +1,28 @@
+package lib
+
+import "database/sql"
+
+func SetupDatabase(db *sql.DB) error {
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS user (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL,
+password TEXT NOT NULL,
+deleted TEXT NOT NULL
+)`)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS session (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+token TEXT NOT NULL,
+user_id INTEGER,
+FOREIGN KEY(user_id) REFERENCES user(id) 
+)`)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 
+	"bsk-bank/lib"
 	"bsk-bank/views"
-	"github.com/a-h/templ"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/libsql/libsql-client-go/libsql"
 )
 
 func main() {
-	component := views.Hello("John")
+	godotenv.Load()
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		fmt.Fprintf(os.Stderr, "DATABASE_URL env var must be set")
+		os.Exit(1)
+	}
 
-	http.Handle("/", templ.Handler(component))
+	db, err := sql.Open("libsql", databaseUrl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not connect to DB %s (%s)", databaseUrl, err)
+		os.Exit(1)
+	}
 
-	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+	// migrations etc
+	lib.SetupDatabase(db)
+
+	application := gin.Default()
+	application.HTMLRender = &lib.TemplRender{}
+
+	application.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "", views.Hello("jon"))
+	})
+
+	application.GET("/login", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "", views.Login())
+	})
+
+	application.POST("/login", func(ctx *gin.Context) {
+		username := ctx.PostForm("username")
+		password := ctx.PostForm("password")
+		ctx.HTML(http.StatusOK, "", views.AfterLogin(username, password))
+	})
+
+	application.Run(":8080")
 }
-
